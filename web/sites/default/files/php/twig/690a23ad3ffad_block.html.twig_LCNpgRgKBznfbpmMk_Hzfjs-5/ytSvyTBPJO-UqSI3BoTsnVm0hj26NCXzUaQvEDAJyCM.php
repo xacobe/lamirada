f63c363e96e5dc7a3c16fffae314d692f661ba3f
xacobe <xacobe.net@gmail.com>
2025-11-04 17:05:13 +0100

    update after 2nd try

---
+++ web/sites/default/files/php/twig/690a23ad3ffad_block.html.twig_LCNpgRgKBznfbpmMk_Hzfjs-5/ytSvyTBPJO-UqSI3BoTsnVm0hj26NCXzUaQvEDAJyCM.php
@@ -0,0 +1,154 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* block.html.twig */
+class __TwigTemplate_f365577e1b3783c573bbb371de5d2def extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'content' => [$this, 'block_content'],
+        ];
+        $this->sandbox = $this->extensions[SandboxExtension::class];
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 30
+        $context["classes"] = ["block", "position-relative", \Drupal\Component\Utility\Html::getClass(CoreExtension::getAttribute($this->env, $this->source,         // line 33
+($context["configuration"] ?? null), "provider", [], "any", false, false, true, 33)), \Drupal\Component\Utility\Html::getClass(        // line 34
+($context["plugin_id"] ?? null))];
+        // line 37
+        yield "<div";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->extensions['Drupal\gin_lb\TwigExtension\GinLbExtension']->escapeFilter($this->env, CoreExtension::getAttribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [($context["classes"] ?? null)], "method", false, false, true, 37), "html", null, true));
+        yield ">
+  ";
+        // line 38
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->extensions['Drupal\gin_lb\TwigExtension\GinLbExtension']->escapeFilter($this->env, ($context["title_prefix"] ?? null), "html", null, true));
+        yield "
+  ";
+        // line 39
+        if (($context["label"] ?? null)) {
+            // line 40
+            yield "    <h2";
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->extensions['Drupal\gin_lb\TwigExtension\GinLbExtension']->escapeFilter($this->env, ($context["title_attributes"] ?? null), "html", null, true));
+            yield ">";
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->extensions['Drupal\gin_lb\TwigExtension\GinLbExtension']->escapeFilter($this->env, ($context["label"] ?? null), "html", null, true));
+            yield "</h2>
+  ";
+        }
+        // line 42
+        yield "  ";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->extensions['Drupal\gin_lb\TwigExtension\GinLbExtension']->escapeFilter($this->env, ($context["title_suffix"] ?? null), "html", null, true));
+        yield "
+  ";
+        // line 43
+        yield from $this->unwrap()->yieldBlock('content', $context, $blocks);
+        // line 46
+        yield "</div>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["configuration", "plugin_id", "attributes", "title_prefix", "label", "title_attributes", "title_suffix", "content"]);        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["configuration", "plugin_id", "attributes", "title_prefix", "label", "title_attributes", "title_suffix", "content"]);        yield from [];
+    }
+
+    // line 43
+    /**
+     * @return iterable<null|scalar|\Stringable>
+     */
+    public function block_content(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        // line 44
+        yield "    ";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar($this->extensions['Drupal\gin_lb\TwigExtension\GinLbExtension']->escapeFilter($this->env, ($context["content"] ?? null), "html", null, true));
+        yield "
+  ";
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "block.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  90 => 44,  83 => 43,  75 => 46,  73 => 43,  68 => 42,  60 => 40,  58 => 39,  54 => 38,  49 => 37,  47 => 34,  46 => 33,  45 => 30,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("", "block.html.twig", "themes/lamirada/templates/block/block.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = ["set" => 30, "if" => 39, "block" => 43];
+        static $filters = ["clean_class" => 33, "escape" => 37];
+        static $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if', 'block'],
+                ['clean_class', 'escape'],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
